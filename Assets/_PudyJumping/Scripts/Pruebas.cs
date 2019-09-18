7f347d0625bfd91fe2a008935718802dd402e94d
jecan <alumno@viralgamingacademy.com>
2019-09-18 13:35:59 +0200

    Fin prototype 02

---
+++ Assets/_PudyJumping/Scripts/Pruebas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pruebas : MonoBehaviour
+{
+    
+    public void CambioValor(float A)
+    {
+        Debug.Log(A);
+    }
+
+    public void OpenBrowser()
+    {
+        Application.OpenURL("http://twitter.com");
+    }
+
+}
